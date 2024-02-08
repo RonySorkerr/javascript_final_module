ad8a253cb73b0a3286d9fc5a79c508090d738dea
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-08 21:39:18 +0600

    latest updates

--- newLoop.js
+++ newLoop.js
@@ -1,5 +1,15 @@
-const array = [1,52,8,2,8,1,8,5];
+// THIS IS THE LATEST WAY TO DO IT
+const array = [1, 52, 8, 2, 8, 1, 8, 5];
 
-for(item of array){
+for (item of array) {
     console.log(item);
+}
+
+// THIS IS THE OLD METHOD TO DO IT******
+
+const numbers = [1000, 8, 5, 82, 8, 2, 8, 2, 9, 54];
+
+for (let i = 0; i< numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
 }
