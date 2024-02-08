a4f818832ed72030bc6870333a39f78f4cb5e731
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-08 21:36:05 +0600

    new style of looping

---
+++ newLoop.js
@@ -0,0 +1,5 @@
+const array = [1,52,8,2,8,1,8,5];
+
+for(item of array){
+    console.log(item);
+}
---
+++ search.js
@@ -0,0 +1,14 @@
+const array = [
+    {id: 1, name: 'xiaomi phone', price: 1500},
+    {id: 2, name: 'xiaomi phone', price: 1500},
+    {id: 3, name: 'xiaomi phone', price: 1500},
+    {id: 4, name: 'xiaomi phone', price: 1500},
+    {id: 5, name: 'xiaomi phone', price: 1500},
+    {id: 6, name: 'xiaomi phone', price: 1500},
+    {id: 7, name: 'xiaomi phone', price: 1500},
+    {id: 8, name: 'xiaomi phone', price: 1500},
+    {id: 9, name: 'xiaomi phone', price: 1500},
+    {id: 10, name: 'xiaomi phone', price: 1500},
+    {id: 11, name: 'xiaomi phone', price: 1500},
+
+]
